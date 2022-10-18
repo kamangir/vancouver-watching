e75753375c5b6df0c756f9b657e08fe627c72946
kamangir <arash@kamangir.net>
2022-10-17 19:13:30 -0700

    validating vanwatch ingest - kamangir/bolt#722

---
+++ .abcli/install/vancouver_watching.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+function abcli_install_vancouver_watching() {
+    pip3 install geojson
+}
+
+abcli_install_module vancouver_watching 101
--- vancouver_watching/__init__.py
+++ vancouver_watching/__init__.py
@@ -1,5 +1,5 @@
 NAME = "vancouver_watching"
 
-VERSION = "2.9.1"
+VERSION = "2.10.1"
 
 from .functions import *
