c5f50b702a4aaa894c290ad6232991dac639acc0
kamangir <arash@kamangir.net>
2022-10-17 20:49:44 -0700

    validating vanwatch ingest - kamangir/bolt#722

--- .abcli/install/vancouver_watching.sh
+++ .abcli/install/vancouver_watching.sh
@@ -3,6 +3,7 @@
 function abcli_install_vancouver_watching() {
     pip3 install geojson
     pip3 install beautifulsoup4
+    pip3 install geopandas
 }
 
-abcli_install_module vancouver_watching 102
+abcli_install_module vancouver_watching 103
--- vancouver_watching/ingest/functions.py
+++ vancouver_watching/ingest/functions.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import geopandas
 import requests
 from tqdm import tqdm
 from abcli import file
@@ -12,16 +13,40 @@ logger = logging.getLogger(__name__)
 def ingest_geojson(filename):
     logger.info(f"{NAME}.ingest_geojson({filename})")
 
-    success, content = file.load_geojson(filename)
-    if not success:
-        return success
+    gdf = geopandas.read_file(filename)
 
-    for feature in tqdm(content["features"]):
-        url = feature["properties"]["url"]
+    list_of_images = []
+    list_of_labels = []
+    err_count = 0
+    for index, row in tqdm(gdf.iterrows()):
+        list_of_images_ = []
 
-        html_page = requests.get(url)
+        try:
+            html_page = requests.get(row["url"])
 
-        # https://towardsdatascience.com/a-tutorial-on-scraping-images-from-the-web-using-beautifulsoup-206a7633e948
-        soup = BeautifulSoup(html_page.content, "html.parser")
+            # https://towardsdatascience.com/a-tutorial-on-scraping-images-from-the-web-using-beautifulsoup-206a7633e948
+            soup = BeautifulSoup(html_page.content, "html.parser")
+            list_of_images_ = [
+                item.attrs["src"]
+                for item in soup.find(
+                    "div",
+                    class_="col-sm-12 section--container",
+                ).findAll("img")
+            ]
+        except:
+            err_count += 1
+            logger.error(f"-vancouver_watching: ingest: {row['url']} failed.")
+
+        list_of_images += [",".join(list_of_images_)]
+        list_of_labels += ['<a href="{}">{}</a><br/> {}'].format(
+            row("url"),
+            row("name"),
+            "<br/> ".join([f'<img src="{image}">' for image in list_of_images_]),
+        )
+
+    gdf["images"] = list_of_images
+    gdf["label"] = list_of_labels
+
+    gdf.to_file(filename, driver="GeoJSON")
 
     return True
