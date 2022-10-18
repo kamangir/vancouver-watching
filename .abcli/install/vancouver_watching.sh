8fd801d1f5d63c361d9f21cbe8dd7eb352b338ef
kamangir <arash@kamangir.net>
2022-10-17 19:23:08 -0700

    validating vanwatch ingest - kamangir/bolt#722

--- .abcli/install/vancouver_watching.sh
+++ .abcli/install/vancouver_watching.sh
@@ -2,6 +2,7 @@
 
 function abcli_install_vancouver_watching() {
     pip3 install geojson
+    pip3 install beautifulsoup4
 }
 
-abcli_install_module vancouver_watching 101
+abcli_install_module vancouver_watching 102
--- vancouver_watching/ingest/functions.py
+++ vancouver_watching/ingest/functions.py
@@ -1,3 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+from tqdm import tqdm
+from abcli import file
 from . import NAME
 from abcli import logging
 import logging
@@ -7,5 +11,17 @@ logger = logging.getLogger(__name__)
 
 def ingest_geojson(filename):
     logger.info(f"{NAME}.ingest_geojson({filename})")
+
+    success, content = file.load_geojson(filename)
+    if not success:
+        return success
+
+    for feature in tqdm(content["features"]):
+        url = feature["properties"]["url"]
+
+        html_page = requests.get(url)
+
+        # https://towardsdatascience.com/a-tutorial-on-scraping-images-from-the-web-using-beautifulsoup-206a7633e948
+        soup = BeautifulSoup(html_page.content, "html.parser")
 
     return True
