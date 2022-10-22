5f215c779393d07b09b67f6a0bd6291336968a99
kamangir <arash@kamangir.net>
2022-10-22 12:40:51 -0700

    prep for worker ingest - kamangir/bolt#722

--- .abcli/install/vancouver_watching.sh
+++ .abcli/install/vancouver_watching.sh
@@ -4,6 +4,7 @@ function abcli_install_vancouver_watching() {
     pip3 install geojson
     pip3 install beautifulsoup4
     pip3 install geopandas
+    pip3 install tqdm
 }
 
 abcli_install_module vancouver_watching 103
