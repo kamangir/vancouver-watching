bf7c63f3ee2ce03de9db1e4594ddc15d8bea1ab1
kamangir <arash@kamangir.net>
2022-10-23 12:00:12 -0700

    validating vanwatch discover iran - kamangir/bolt#722

--- .abcli/discovery/iran.sh
+++ .abcli/discovery/iran.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env bash
 
 function vancouver_watching_discover_iran() {
-    iran_watching discover $@
+    python3 -m iran_watching \
+        discover_cameras_iran_style \
+        --filename $abcli_object_path/iran.geojson \
+        ${@:2}
 }
