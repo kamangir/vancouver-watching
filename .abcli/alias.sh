d8396d5280e1100cd2f610b39ba9c6a6d9429195
kamangir <arash@kamangir.net>
2023-07-03 18:15:33 -0700

    += roboflow - kamangir/bolt#kamangir/bolt#746

---
+++ .abcli/alias.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+alias @roboflow=vancouver_watching_roboflow
--- vancouver_watching/__init__.py
+++ vancouver_watching/__init__.py
@@ -1,6 +1,6 @@
 NAME = "vancouver_watching"
 
-VERSION = "3.41.1"
+VERSION = "3.42.1"
 
 from .discover import *
 from .ingest import *
