03b43a1dae9ad3fd774483690fb4cf8795e3e85d
kamangir <arash@kamangir.net>
2023-11-04 20:06:41 -0700

    ingest refactor - kamangir/bolt#420

--- .abcli/ingest.sh
+++ .abcli/ingest.sh
@@ -1,11 +1,15 @@
 #! /usr/bin/env bash
 
+# https://hub.ultralytics.com/models/R6nMlK6kQjSsQ76MPqQM?tab=preview
+export vancouver_watching_default_model="R6nMlK6kQjSsQ76MPqQM"
+
 function vancouver_watching_ingest() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ "$task" == "help" ]; then
+        local options="detect,dryrun,model=<$vancouver_watching_default_model>,~upload"
         local args="[--count <-1>]"
-        abcli_show_usage "vancouver_watching ingest$ABCUL<area>$ABCUL[dryrun,~upload]$ABCUL[<object-name>]$ABCUL$args" \
+        abcli_show_usage "vancouver_watching ingest$ABCUL<area>$ABCUL[$options]$ABCUL[<object-name>]$ABCUL$args" \
             "ingest <area> -> <object-name>."
 
         if [ "$(abcli_keyword_is $2 verbose)" == true ]; then
@@ -16,12 +20,13 @@ function vancouver_watching_ingest() {
     local area=$(abcli_clarify_input $1 vancouver)
 
     local options=$2
+    local do_detect=$(abcli_option_int "$options" detect 0)
     local do_dryrun=$(abcli_option_int "$options" dryrun 0)
     local do_upload=$(abcli_option_int "$options" upload $(abcli_not $do_dryrun))
+    local model_id=$(abcli_option "$options" model $vancouver_watching_default_model)
 
     local object_name=$(abcli_clarify_object $3 $(abcli_string_timestamp))
     local object_path=$abcli_object_root/$object_name
-    mkdir -pv $object_path
 
     local discovery_object=$(
         abcli_tag search \
@@ -45,6 +50,14 @@ function vancouver_watching_ingest() {
         --do_dryrun $do_dryrun \
         --filename $object_path/$area.geojson \
         "${@:4}"
+
+    [[ "$do_detect" == 1 ]] &&
+        python3 -m vancouver_watching.ai \
+            detect \
+            --do_dryrun $do_dryrun \
+            --model_id $model_id \
+            --filename $object_path/summary.json \
+            "${@:4}"
 
     abcli_tag set \
         $object_name \
--- vancouver_watching/ingest/functions.py
+++ vancouver_watching/ingest/functions.py
@@ -11,22 +11,28 @@ logger = logging.getLogger(__name__)
 
 
 def ingest_from_cameras(
-    filename: str,
+    cameras_filename: str,
     count: int,
     do_dryrun: bool = False,
 ):
-    success, list_of_cameras = get_list_of_cameras(filename)
+    success, list_of_cameras = get_list_of_cameras(cameras_filename)
     if not success:
         return False
 
     if count != -1:
         list_of_cameras = list_of_cameras[:count]
+    logger.info(
+        "ingest_from_cameras: loaded {} camera(s) from {}.".format(
+            len(list_of_cameras),
+            cameras_filename,
+        )
+    )
 
-    success = True
     failed_urls = []
     p = re.compile(
         "https?:\/\/([0-9.]+).\:([0-9.]+)\/webcapture.jpg.*.channel=([0-9.]+).*"
     )
+    metadata = {}
     for url in tqdm(list_of_cameras):
         matches = p.match(url)
         if matches:
@@ -45,13 +51,16 @@ def ingest_from_cameras(
             logger.info(url)
         elif not file.download(
             url,
-            os.path.join(
-                os.getenv("abcli_object_path"),
-                filename,
-            ),
+            os.path.join(os.getenv("abcli_object_path"), filename),
         ):
-            success = False
-    if failed_urls:
-        logger.error(f"{len(failed_urls)} url(s) failed.")
+            logger.error(f"bad url: {url}.")
+            failed_urls += [url]
+            continue
 
-    return success
+        metadata[filename(filename)] = {"url": url}
+    if failed_urls:
+        logger.error("{} url(s) failed.".format(len(failed_urls)))
+
+    metadata_filename = file.set_extension(cameras_filename, "json")
+    logger.info("{} images ingested -> {}".format(len(metadata), metadata_filename))
+    return file.save_json(metadata_filename, metadata)
