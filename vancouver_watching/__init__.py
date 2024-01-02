2f2ac444d5092574c3547864154bb8e1244b25af
kamangir <arash@kamangir.net>
2024-01-01 18:37:16 -0800

    += vanwatch ingest - kamangir/bolt#123

--- vancouver_watching/__init__.py
+++ vancouver_watching/__init__.py
@@ -1,5 +1,5 @@
 NAME = "vancouver_watching"
 
-VERSION = "3.172.1"
+VERSION = "3.174.1"
 
-DESCRIPTION = "🌈 bird watching in downtown Vancouver with AI."
+DESCRIPTION = "🌈 Vancouver Watching with AI."
