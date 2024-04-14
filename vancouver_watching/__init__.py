975d274e8f34259c99b71ca35b1967368c07806d
kamangir <arash@kamangir.net>
2024-04-14 16:07:27 -0700

    pipeline refactor - kamangir/bolt#746

--- vancouver_watching/__init__.py
+++ vancouver_watching/__init__.py
@@ -4,4 +4,4 @@ ICON = "🌈"
 
 DESCRIPTION = f"{ICON} Vancouver Watching with AI."
 
-VERSION = "3.320.1"
+VERSION = "3.321.1"
---
+++ vancouver_watching/tests/test_env.py
@@ -0,0 +1,5 @@
+from abcli.tests import test_env
+
+
+def test_abcli_env():
+    test_env.test_abcli_env()
