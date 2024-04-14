aa2331c062cb05552799b0184e8fc59e92190642
kamangir <arash@kamangir.net>
2024-04-14 16:14:26 -0700

    pipeline refactor - kamangir/bolt#746

--- vancouver_watching/tests/test_env.py
+++ vancouver_watching/tests/test_env.py
@@ -1,5 +1,11 @@
-from abcli.tests import test_env
+from abcli import env
 
 
 def test_abcli_env():
-    test_env.test_abcli_env()
+    assert env.abcli_path_home
+    assert env.abcli_path_git
+    assert env.abcli_path_storage
+
+    assert env.ABCLI_AWS_RDS_DB
+    assert env.ABCLI_AWS_RDS_PORT
+    assert env.ABCLI_AWS_RDS_USER
