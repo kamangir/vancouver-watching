d82370c71b881cfbf00559267d463bf05d9e9e7c
kamangir <arash@kamangir.net>
2024-05-10 19:20:42 -0700

    test -> README.md 🚀 (hopefully) - kamangir/bolt#746

--- .abcli/tests/ingest.sh
+++ .abcli/tests/ingest.sh
@@ -3,8 +3,15 @@
 function test_vancouver_watching_ingest() {
     local options=$1
 
+    local object_name="test_vancouver_watching_ingest_$(abcli_string_timestamp)"
+
     abcli_eval ,$options \
         vanwatch ingest \
         area=vancouver,count=3,~batch,$2 \
+        $object_name \
         "${@:3}"
+
+    abcli_publish \
+        as=test_vancouver_watching_ingest,~download,suffix=.gif \
+        $object_name
 }
