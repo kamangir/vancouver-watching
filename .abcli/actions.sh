3cbb53d1ed96abb67153dbd426ed730cb5cae416
kamangir <arash@kamangir.net>
2024-05-25 10:53:25 -0700

    += action - kamangir/bolt#746

---
+++ .abcli/actions.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+function vancouver_watching_action_git_before_push() {
+    vancouver_watching pypi build
+}
--- vancouver_watching/__init__.py
+++ vancouver_watching/__init__.py
@@ -4,4 +4,4 @@ ICON = "🌈"
 
 DESCRIPTION = f"{ICON} Vancouver Watching with AI."
 
-VERSION = "3.348.1"
+VERSION = "3.349.1"
