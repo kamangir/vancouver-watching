bd312fe024665b095232cb73bfeaf97f4bb94ad8
kamangir <arash@kamangir.net>
2024-06-15 22:30:07 -0700

    += marquee - kamangir/bolt#746

--- vancouver_watching/__init__.py
+++ vancouver_watching/__init__.py
@@ -4,6 +4,8 @@ ICON = "🌈"
 
 DESCRIPTION = f"{ICON} Vancouver Watching with AI."
 
-VERSION = "3.372.1"
+VERSION = "3.373.1"
 
 REPO_NAME = "vancouver-watching"
+
+MARQUEE = "https://user-images.githubusercontent.com/1007567/196573547-b1c71b3b-7fac-4d2c-bba0-a87b063830da.png"
