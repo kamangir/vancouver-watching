89b54bcffce1e4ebf5a9a4a9bdd2c20fd51dd5f7
kamangir <arash@kamangir.net>
2024-08-24 13:08:21 -0700

    doc refactor - kamangir/bolt#746

--- vancouver_watching/__main__.py
+++ vancouver_watching/__main__.py
@@ -2,12 +2,11 @@ from vancouver_watching import NAME, VERSION, DESCRIPTION, ICON
 from vancouver_watching.logger import logger
 from blueness.argparse.generic import main
 
-success, message = main(
-    __file__,
-    NAME,
-    VERSION,
-    DESCRIPTION,
-    ICON,
+main(
+    ICON=ICON,
+    NAME=NAME,
+    DESCRIPTION=DESCRIPTION,
+    VERSION=VERSION,
+    main_filename=__file__,
+    logger=logger,
 )
-if not success:
-    logger.error(message)
