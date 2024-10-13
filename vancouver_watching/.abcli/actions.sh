b501d7b685434a82e855a0893f96de0dd3f9ee28
kamangir <arash@kamangir.net>
2024-10-13 13:03:41 -0700

    git refactor - kamangir/bolt#746

--- vancouver_watching/.abcli/actions.sh
+++ vancouver_watching/.abcli/actions.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
 function vancouver_watching_action_git_before_push() {
-    [[ "$(abcli_git get_branch)" == "main" ]] &&
-        vancouver_watching pypi build
+    [[ "$(abcli_git get_branch)" != "main" ]] &&
+        return 0
+
+    vancouver_watching pypi build
 }
