8c292535305aff6ddf935fab411b6565f10770ea
kamangir <arash@kamangir.net>
2025-05-02 11:06:42 -0400

    ++ - kamangir/bolt#746

--- vancouver_watching/.abcli/tests/discover_ingest_and_detect.sh
+++ vancouver_watching/.abcli/tests/discover_ingest_and_detect.sh
@@ -3,20 +3,29 @@
 function test_vancouver_watching_discover_ingest_and_detect() {
     local options=$1
 
-    local object_name=test_vancouver_watching_discover-$(bluer_ai_string_timestamp_short)
+    local list_of_targets=$(vancouver_watching_list_targets)
+    list_of_targets=$(bluer_ai_option "$options" target $list_of_targets)
 
-    bluer_ai_eval ,$options \
-        vancouver_watching \
-        discover \
-        target=vancouver,count=3,~upload \
-        $object_name
-    [[ $? -ne 0 ]] && return 1
+    local target
+    for target in $(echo "$list_of_targets" | tr + " "); do
+        bluer_ai_log "🎯 target: $target..."
 
-    local object_name=test_vancouver_watching_discover_ingest_and_detect-$(bluer_ai_string_timestamp_short)
+        local object_name=test_vancouver_watching_discover-$target-$(bluer_ai_string_timestamp_short)
 
-    vancouver_watching ingest \
-        target=vancouver,count=3,~upload,$2 \
-        $object_name \
-        detect,gif,~download,$3 \
-        "${@:4}"
+        bluer_ai_eval ,$options \
+            vancouver_watching \
+            discover \
+            target=$target,count=3,~upload \
+            $object_name
+        [[ $? -ne 0 ]] && return 1
+
+        local object_name=test_vancouver_watching_discover_ingest_and_detect-$target-$(bluer_ai_string_timestamp_short)
+
+        vancouver_watching ingest \
+            target=$target,count=3,~upload,$2 \
+            $object_name \
+            detect,gif,~download,$3 \
+            "${@:4}"
+    done
+    return 0
 }
